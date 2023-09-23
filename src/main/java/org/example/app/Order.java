26160c46ee25bffe0e12c1ba65d6d106414a9338
DawidNst <dawidniesterczuk@gmail.com>
2023-09-23 19:37:37 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/app/Order.java
+++ src/main/java/org/example/app/Order.java
@@ -40,6 +40,8 @@ public class Order {
                 '}';
     }
 
+    public static class OrderBackup {
+    }
 }
 
 
---
+++ src/test/java/org/example/app/OrderBackupTest.java
@@ -0,0 +1,34 @@
+package org.example.app;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderBackupTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getWriter() {
+    }
+
+    @Test
+    void createFile() {
+    }
+
+    @Test
+    void backupOder() {
+    }
+
+    @Test
+    void closeFile() {
+    }
+}
