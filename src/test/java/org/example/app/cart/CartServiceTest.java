04b3eca03892e3f83210f9428d8a447d010f4468
DawidNst <dawidniesterczuk@gmail.com>
2023-12-13 15:47:49 +0100

    commit

--- src/test/java/org/example/app/cart/CartServiceTest.java
+++ src/test/java/org/example/app/cart/CartServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class CartServiceTest {
 
@@ -17,10 +20,18 @@ class CartServiceTest {
         Cart cart = new Cart();
         cart.addOrderToCart(order);
 
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
 
+        given(cartHandler.canHandlerCart(cart)).willReturn(true);
 
         //when
 
+        Cart  resultCart = cartService.processCart(cart);
+
         //then
+
+        verify(cartHandler).sendToPrepare(cart);
+
     }
 }
