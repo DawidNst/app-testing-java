a6ff40abd557f0ad69f045c072e1f1110152e093
DawidNst <dawidniesterczuk@gmail.com>
2023-12-15 14:13:26 +0100

    commit

--- src/test/java/org/example/app/cart/CartServiceTest.java
+++ src/test/java/org/example/app/cart/CartServiceTest.java
@@ -28,7 +28,7 @@ class CartServiceTest {
 
         //when
 
-        Cart  resultCart = cartService.processCart(cart);
+        Cart resultCart = cartService.processCart(cart);
 
         //then
 
@@ -54,12 +54,62 @@ class CartServiceTest {
 
         //when
 
-        Cart  resultCart = cartService.processCart(cart);
+        Cart resultCart = cartService.processCart(cart);
 
         //then
 
         verify(cartHandler, never()).sendToPrepare(cart);
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.REJECTED));
+
+    }
+
+    @Test
+    void processCartShouldBeNotPropertArgumentMatchers() {
+
+        //given
+
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandlerCart(any(Cart.class))).willReturn(false);
+
+        //when
+
+        Cart  resultCart = cartService.processCart(cart);
+
+        //then
+
+        verify(cartHandler, never()).sendToPrepare(any(Cart.class));
         assertThat(resultCart.getOrders().get(0).getOrderStatus(),equalTo(OrderStatus.REJECTED));
-       
+
+    }
+
+    @Test
+    void processCartShouldBeNotPropertArgumentMatchers() {
+
+        //given
+
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandlerCart(any(Cart.class))).willReturn(false);
+
+        //when
+
+        Cart  resultCart = cartService.processCart(cart);
+
+        //then
+
+        verify(cartHandler, never()).sendToPrepare(any(Cart.class));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(),equalTo(OrderStatus.REJECTED));
+
     }
 }
