8dd4e8eb6c3a456a896f738e65daaf4fed433aec
DawidNst <dawidniesterczuk@gmail.com>
2023-12-23 16:10:05 +0100

    Your commit

--- src/main/java/org/example/app/MealRepository.java
+++ src/main/java/org/example/app/MealRepository.java
@@ -1,6 +1,17 @@
 package org.example.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MealRepository {
+
+    private List<Meal> meals = new ArrayList<>();
+
     public void add(Meal meal) {
+        meals.add(meal);
+    }
+
+    public List<Meal> getAllMeals() {
+        return meals;
     }
 }
--- src/test/java/org/example/app/MealRepositoryTest.java
+++ src/test/java/org/example/app/MealRepositoryTest.java
@@ -2,18 +2,22 @@ package org.example.app;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class MealRepositoryTest {
 
     @Test
     void shouldBeAbelToAddMealToRepository() {
 
         //given
-
         MealRepository mealRepository = new MealRepository();
-        Meal meal = new Meal(20,"Pizza");
+        Meal meal = new Meal(20, "Pizza");
 
         //when
-
         mealRepository.add(meal);
+
+        //then
+        assertThat(mealRepository.getAllMeals().get(0),is(meal));
     }
 }
